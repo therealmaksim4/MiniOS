a2461f36f88cc80d177c9f529b86f3682742bb9a
therealmaksim4 <maksim.budim11052013@gmail.com>
2026-07-04 19:34:26 +0200

    Added CMakeLists.txt, src/ and build/

---
+++ build/CMakeFiles/minios.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minios.dir/link.d"
+  "CMakeFiles/minios.dir/src/main.cpp.o"
+  "CMakeFiles/minios.dir/src/main.cpp.o.d"
+  "minios"
+  "minios.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minios.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
